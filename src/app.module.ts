cb2ee853459e401562ea03c086290264e24ec4cf
Bhavya56s <dhruvsingal200@gmail.com>
2024-07-15 10:02:15 +0530

    setup

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { UsersControllers } from './user.controller';
+
+
+@Module({
+ 
+  controllers: [UsersControllers],
+  
+})
+export class AppModule {}
